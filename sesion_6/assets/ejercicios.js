0d6b80b93596fe842616075c6fb8a5322d6fb2bc
Emiliano Rotta <emilianorottaf@gmail.com>
2024-10-01 20:21:42 -0300

    Ejercicios para hacer

---
+++ sesion_6/assets/ejercicios.js
@@ -0,0 +1,41 @@
+// Callback
+
+// // 1 Crea una función mostrarMensaje que reciba un mensaje y lo muestre en un alert(). Luego, crea otra función solicitarMensaje que pida al usuario que ingrese un mensaje usando prompt(), y que llame a mostrarMensaje con el valor ingresado.
+
+// // 2 Implementa una función multiplicarPorDos que reciba un número y muestre el resultado en un alert(). Luego, crea una función pedirNumero que solicite un número al usuario con prompt() y pase ese número a multiplicarPorDos mediante un callback.
+
+// // 3 Crea una función mostrarFecha que muestre la fecha actual en un alert(). Luego, implementa otra función pedirConfirmacion que le pregunte al usuario si desea ver la fecha usando confirm(). Si el usuario acepta, llama a mostrarFecha mediante un callback.
+
+// // 4 Escribe una función convertirMayusculas que reciba una cadena de texto y la convierta a mayúsculas, luego muestre el resultado en un alert(). Crea también la función solicitarTexto que pida al usuario que ingrese un texto con prompt() y luego pase ese texto a convertirMayusculas mediante un callback.
+
+// // 5 Crea una función calcularEdad que reciba el año de nacimiento del usuario y calcule su edad actual, mostrando el resultado en un alert(). Luego, crea una función solicitarNacimiento que pida el año de nacimiento al usuario y llame a calcularEdad mediante un callback.
+
+//-------------------------------------------------------
+//setTimeout()
+
+// Consignas:
+
+//1 Crea una función que imprima "¡Hola!" después de 2 segundos.
+
+//2 Implementa un temporizador que imprima "Tiempo agotado" después de 5 segundos.
+
+//3 Escribe un código que imprima "Inicio", luego "Medio" después de 3 segundos, y finalmente "Fin" después de 5 segundos.
+
+
+//-------------------------------------------------------
+//SetInterval()
+// Consignas:
+
+// 1 Crea un temporizador que imprima "Ejecutando..." cada 3 segundos.
+
+// 2 Haz un contador que se incremente cada 1 segundo e imprima el valor.
+
+//-------------------------------------------------------------------------
+//clearInterval()
+
+// Ejercicio 1: Detener un contador
+// Crea un contador que se incrementa cada segundo. Cuando el contador llegue a 10, el intervalo debe detenerse.
+
+// Ejercicio 2: Temporizador que se detiene manualmente
+// Crea un temporizador que se ejecute cada segundo e imprima "Temporizador activo". Usa un botón para detener el temporizador al hacer clic.
+
